a29c1216c370373fa8b15aa6d17fa9abfa7d785d
Yana Bashtyk <Yana_Bashtyk@epam.com>
2019-02-25 19:27:49 +0300

    feat: add build folder

---
+++ precache-manifest.65b86fbafe7437ac03b044e7243c28c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdf1bc1831df56f2006e",
+    "url": "/dashboard-ghpages/static/js/runtime~main.bdf1bc18.js"
+  },
+  {
+    "revision": "6c569f8a5697bc9beb6b",
+    "url": "/dashboard-ghpages/static/js/main.6c569f8a.chunk.js"
+  },
+  {
+    "revision": "e2c48e8053eba71acff5",
+    "url": "/dashboard-ghpages/static/js/2.e2c48e80.chunk.js"
+  },
+  {
+    "revision": "6c569f8a5697bc9beb6b",
+    "url": "/dashboard-ghpages/static/css/main.e9e42ab1.chunk.css"
+  },
+  {
+    "revision": "e2c48e8053eba71acff5",
+    "url": "/dashboard-ghpages/static/css/2.0978c9ff.chunk.css"
+  },
+  {
+    "revision": "27e91fc94fb31dbeffcfa781b6335df0",
+    "url": "/dashboard-ghpages/index.html"
+  }
+];
